6c261fc6770a438434627cbc836554821e9ba1ae
getulio <augustoge71@gmail.com>
2022-03-06 17:43:23 -0300

    iniciando as configurações de envio de e-mail em lista

--- enviaemail.py
+++ enviaemail.py
@@ -1,3 +1,28 @@
 import smtplib
 import email.message
+import pandas as pd
+import openpyxl
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+lista_clientes = pd.read_excel('C:\\Users\\augus\\Documents\\projetos\\lista_clientes.xlsx')
+print(lista_clientes)
+cliente = lista_clientes['CLIENTE']
+mat_cliente = lista_clientes['MATRÍCULA']
+email_cliente = lista_clientes['E-MAIL']
+
+# startando o servidor SMTP
+host = 'smtp.gmail.com'
+port = '587'
+login = 'augustoge71@gmail.com'
+senha = 'Getdev1971'
+server = smtplib.SMTP(host, port)
+
+server.ehlo()
+server.starttls()
+server.login(login, senha)
+
+# construindo e-mail tipo MIME
+
+
 
